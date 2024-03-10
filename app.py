c8fddf16e285be00f2a7d92c089dc0614abc52b9
Zombiepro89 <corymyers2003@gmail.com>
2024-03-10 16:02:43 -0500

    Added a very barebones version of the main chat screen. The three frames will need to be expanded on in the future.

--- app.py
+++ app.py
@@ -12,15 +12,30 @@ def display_message():
 
 
 app = tk.Tk()
-app.title("Local Application")
+app.title("Discord Mobile Demo")
 
 
 app.minsize(width=WIDTH_SCREEN, height=HEIGHT_SCREEN)
 
-chatFrame = tk.Frame(app)
-chatFrame.grid(row=0, column=0)
+channelFrame = tk.Frame(app, highlightthickness=3, highlightbackground="black")
+channelFrame.grid(row=0, column=0)
 
-testLabel = tk.Label(chatFrame, text= "Hello world!")
+# After some testing, a width of 17 allows the label to span the screen without making the window too much bigger
+channelName = tk.Label(channelFrame, text= "# Channel", font=("Arial", 25), width=17)
+channelName.grid(row=0, column=1)
+
+# This is where all the chat messages will be displayed.
+chatFrame = tk.Frame(app, highlightthickness=3, highlightbackground="gray")
+chatFrame.grid(row=1, column=0)
+
+testLabel = tk.Label(chatFrame, text= "Hello world!", width=46, height=39)
 testLabel.grid(row=0, column=0)
+
+# This is where the user will type their message before sending.
+messageFrame = tk.Frame(app)
+messageFrame.grid(row=2, column=0)
+
+tk.Label(messageFrame, text="Type your message here!").grid(row=0, column=0)
+
 
 app.mainloop()
