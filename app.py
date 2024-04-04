523b54f358d73cf469af2f54cfeab5835ec680eb
emilyridge <106283484+emilyridge@users.noreply.github.com>
2024-04-04 18:13:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from tkinter import messagebox, font, ttk
 import tkinter as tk
 from tkinter import messagebox
+from models import Message
 
 def display_message():
     messagebox.showinfo("Message", "Hello this is a local application")
